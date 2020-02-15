eea829530fd4e047027f3ef5dc5dd677cd0f2342
GLSharpe <43884592+GLSharpe@users.noreply.github.com>
2020-02-15 17:12:00 +0000

    removed unneeded code

--- src/java/com.company/DriverBuilder.java
+++ src/java/com.company/DriverBuilder.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DriverBuilder {
 
     public WebDriver getDriver(){
-        System.getProperty("Dwebdriver.chrome.driver","C:\\Users\\George.Sharpe\\Documents\\Selenium\\chromedirver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://skyronic.github.io/vue-spa/#/");
         return driver;
